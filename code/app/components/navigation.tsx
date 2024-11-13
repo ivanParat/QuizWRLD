8cf5dc9d045c1c40fdfac0b8ee0cf303c5fde727
ivanParat <ivan.parat2003@gmail.com>
2024-11-13 20:16:31 +0100

    Initialized next.js project and established routes between template pages

---
+++ code/app/components/navigation.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+type Page = {
+  title: string;
+  path: `/${string}`;
+};
+
+const pages: Page[] = [
+  { title: "Home", path: "/" },
+  {
+    title: "Discover Quizzes",
+    path: "/discover-quizzes",
+  },
+  {
+    title: "My Quizzes",
+    path: "/my-quizzes",
+  },
+  {
+    title: "About us",
+    path: "/about",
+  },  
+  {
+    title: "Log in",
+    path: "/login",
+  },
+  {
+    title: "Profile",
+    path: "/profile",
+  },
+
+];
+
+function processPage(page: Page, index: number, pathname: string) {
+  return (
+    <li key={index}>
+      <Link
+        href={page.path}
+        className={
+          page.path === "/"
+            ? pathname === page.path
+              ? "font-extrabold"
+              : ""
+            : pathname.startsWith(page.path)
+            ? "font-extrabold"
+            : ""
+        }
+      >
+        {page.title}
+      </Link>
+    </li>
+  );
+}
+
+export function Navigation() {
+  const pathname = usePathname();
+  return (
+    <ul className="flex justify-center space-x-4 mt-8">
+      {pages.map((page, index) => processPage(page, index, pathname))}
+    </ul>
+  );
+}
---
+++ code/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { Navigation } from "./components/navigation";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "QuizWRLD",
+  description: "Create or solve fun and challenging quizzes instantly.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Navigation></Navigation>
+        {children}
+      </body>
+    </html>
+  );
+}
