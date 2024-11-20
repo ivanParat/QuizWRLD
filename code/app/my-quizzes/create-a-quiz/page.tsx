7cb42ac1a0644cda37a4de7680a5fec8f6109374
Tin922 <tinjuric92@gmail.com>
2024-11-20 19:35:47 +0100

    corrected function name and text

--- code/app/my-quizzes/create-a-quiz/page.tsx
+++ code/app/my-quizzes/create-a-quiz/page.tsx
@@ -1,4 +1,4 @@
-export default function QuizRecommendationPage() {
+export default function CreateAQuizPage() {
   return (
     <main className="flex min-h-screen flex-col items-center p-10">
       <h1 className="text-4xl font-extrabold tracking-tight">Create a Quiz</h1>
