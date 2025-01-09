1490e748913a89359dba827bb33a89e80867adf1
ivanParat <ivan.parat2003@gmail.com>
2025-01-09 20:23:20 +0100

    Added colors to tailwind.config.ts

--- code/tailwind.config.ts
+++ code/tailwind.config.ts
@@ -11,6 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        brand: {
+          DEFAULT: '#007BFF', 
+          light: '#B1D7FF',  
+          dark: '#243648',   
+        },
+        'accent': 'FFC107',
+        'off-white': 'F8F9FA',
+        'main-text': '343A40',
+        'secondary-text': '6C757D',
+        'correct': '28A745',
+        'incorrect': 'DC3545',
       },
     },
   },
