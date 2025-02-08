f7a5f739f131fa413cd1e4ff9b52a3f96bd1d667
Tin922 <tinjuric92@gmail.com>
2025-02-08 18:23:56 +0100

    added api calls

--- code/app/lib/api.ts
+++ code/app/lib/api.ts
@@ -1,5 +1,7 @@
+"use server";
 import { unstable_cache } from "next/cache";
 import client from "./contentfulClient";
+import { TypeQuizzSkeleton, TypeQuizzHomePageSkeleton } from "../content-types";
 
 const MINUTE = 60;
 const HOUR = 60 * MINUTE;
@@ -23,4 +25,58 @@ export const getMainNavigation = unstable_cache(
   },
   ["navigation"],
   { revalidate: HOUR, tags: ["navigation"] }
+);
+
+export const getQuizzes = unstable_cache(
+  async () => {
+    try {
+      const data =
+        await client.withoutUnresolvableLinks.getEntries<TypeQuizzSkeleton>({
+          content_type: "quiz",
+          select: [
+            "fields.title",
+            "fields.slug",
+            "fields.description",
+            "fields.heroImage",
+            "fields.category",
+            "fields.questions",
+            "fields.rating",
+          ],
+        });
+
+      return data.items;
+    } catch (error) {
+      console.error("Error fetching quizzes:", error);
+      return [];
+    }
+  },
+  ["quizzes"],
+  { revalidate: HOUR, tags: ["quizzes"] }
+);
+
+export const getQuizzesHomePage = unstable_cache(
+  async () => {
+    try {
+      const data =
+        await client.withoutUnresolvableLinks.getEntries<TypeQuizzHomePageSkeleton>(
+          {
+            content_type: "quiz",
+            select: [
+              "fields.title",
+              "fields.slug",
+              "fields.heroImage",
+              "fields.category",
+              "fields.rating",
+            ],
+          }
+        );
+
+      return data.items;
+    } catch (error) {
+      console.error("Error fetching quizzes:", error);
+      return [];
+    }
+  },
+  ["quizzes"],
+  { revalidate: HOUR, tags: ["quizzes"] }
 );
