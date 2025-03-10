5117a545c2966acb4303ef27d7345eddb3194e91
Tin922 <tinjuric92@gmail.com>
2025-03-10 21:51:31 +0100

    added signin for desktop

--- code/app/login/page.tsx
+++ code/app/login/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { authClient } from "../lib/auth-client";
+import Link from "next/link";
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -37,36 +38,47 @@ export default function Login() {
 
   return (
     <div className="flex flex-1 items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+      <div className="max-w-md w-full space-y-8 ">
         <h2 className="text-center text-3xl font-extrabold text-gray-900">
-          Log in to your account
+          Log In to Your Account
         </h2>
-        <form className="mt-8 space-y-6" onSubmit={login}>
-          <div className="space-y-4">
+        <form
+          className="space-y-1 bg-background-form px-10 py-10 rounded-xl"
+          onSubmit={login}
+        >
+          <div>Email</div>
+          <div>
             <input
               type="email"
               placeholder="Email address"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-3 py-2 border rounded-md"
+              className="w-full px-3 py-2 mb-3 border rounded-md"
               required
             />
+            <div>Password</div>
             <input
               type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded-md"
+              className="w-full px-3 py-2 mb-3 border rounded-md"
               required
             />
           </div>
           <button
             type="submit"
             disabled={loading}
-            className="w-full py-2 px-4 bg-indigo-600 text-white rounded-md hover:bg-indigo-700"
+            className="w-28 py-2 px-4 bg-brand text-white rounded-md hover:bg-light"
           >
             {loading ? "Logging In..." : "Log In"}
           </button>
+          <div className="text-secondary-text">
+            Don't have an account?{" "}
+            <Link href="/signup" className="text-brand">
+              Sign up
+            </Link>
+          </div>
         </form>
       </div>
     </div>
