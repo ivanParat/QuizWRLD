fc09825498b89d5b5b5c1db3a2bf4867e05ba907
Tin922 <tinjuric92@gmail.com>
2025-05-16 20:16:54 +0200

    added loading component for about us page

---
+++ code/app/about/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-off-white">
+      <div className="text-xl text-main-text animate-pulse">Loading...</div>
+    </main>
+  );
+}
