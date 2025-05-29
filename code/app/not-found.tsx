b6eae05fe350d96a72bebbb60d7c9bd5ceaeedd0
ivanParat <ivan.parat2003@gmail.com>
2025-05-29 17:55:29 +0200

    not found

---
+++ code/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function NotFound() {
+ return (
+  <div className="flex justify-center items-center flex-grow text-main-text min-h-screen bg-off-white">
+    <div className="flex flex-col space-y-4 text-xl font-medium items-center">
+      <h2 className="text-4xl">404 - Not Found</h2>
+      <p>Page does not exist!</p>
+      <Link href='/' className="hover:text-brand active:text-brand">Go back to home page</Link>
+    </div>
+  </div>
+ );
+}
