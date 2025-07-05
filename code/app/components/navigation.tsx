92c7f210c8284e7443f47570b1fa5d2fcc30aee7
Tin922 <tinjuric92@gmail.com>
2025-07-05 17:52:57 +0200

    now fetching navigation from contentful

--- code/app/components/navigation.tsx
+++ code/app/components/navigation.tsx
@@ -9,11 +9,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import Image from "next/image";
 import Cookies from "js-cookie";
 import { authClient } from "../lib/auth-client";
-
-type Page = {
-  title: string;
-  path: `/${string}`;
-};
+import { NavigationItem } from "../content-types";
 
 type User = {
   id: string | null;
@@ -21,50 +17,30 @@ type User = {
   profilePicture: string | null;
 } | null;
 
-const pages: Page[] = [
-  { title: "Home", path: "/" },
-  {
-    title: "Discover Quizzes",
-    path: "/discover-quizzes/quizzes",
-  },
-  {
-    title: "My Quizzes",
-    path: "/my-quizzes",
-  },
-  {
-    title: "About us",
-    path: "/about",
-  },
-  {
-    title: "Blog",
-    path: "/blog",
-  },
-  {
-    title: "Log in",
-    path: "/login",
-  },
-];
-
-function processPage(page: Page, index: number, onClick?: () => void) {
+function processPage(
+  page: NavigationItem,
+  index: number,
+  onClick?: () => void
+) {
   return (
     <li key={index}>
-      {page.path === "/login" ? (
+      {page.fields.path === "/login" ? (
         <Link
-          href={page.path}
+          href={page.fields.path}
           onClick={onClick}
           className="font-bold md:ml-1 lg:ml-3 xl:ml-5"
         >
           <button className="bg-brand text-white px-6 py-1 rounded-md sm:hover:bg-brand-hover sm:active:bg-brand-hover">
-            {page.title}
+            {page.fields.title}
           </button>
         </Link>
       ) : (
         <Link
-          href={page.path}
+          href={page.fields.path}
           onClick={onClick}
           className="font-bold sm:hover:text-brand sm:active:text-brand px-5 py-1 md:px-1 lg:px-3 xl:px-5"
         >
-          {page.title}
+          {page.fields.title}
         </Link>
       )}
     </li>
@@ -231,78 +207,118 @@ function Hamburger({ isOpen, toggleMenu }: HamburgerProps) {
   );
 }
 
-function ProfilePicture({user, onClick}: {user: User, onClick: () => void}){
-  if(user) return(
-    <li className="flex items-center gap-2 min-w-8">
-      <Image
-        src={user.profilePicture ?? "/images/default-profile-picture.svg"}
-        alt={user.name ?? "User"}
-        width={300}
-        height={300}
-        className="w-8 h-8 rounded-full object-cover cursor-pointer"
-        onClick={onClick}
-        priority={true}
-      />
-    </li>
-  );
+function ProfilePicture({
+  user,
+  onClick,
+}: {
+  user: User;
+  onClick: () => void;
+}) {
+  if (user)
+    return (
+      <li className="flex items-center gap-2 min-w-8">
+        <Image
+          src={user.profilePicture ?? "/images/default-profile-picture.svg"}
+          alt={user.name ?? "User"}
+          width={300}
+          height={300}
+          className="w-8 h-8 rounded-full object-cover cursor-pointer"
+          onClick={onClick}
+          priority={true}
+        />
+      </li>
+    );
   return null;
 }
 
-function ProfileDropdown({user, open, closeMenu}: {user: User, open: boolean, closeMenu: () => void}){
-  if(user)
-  return(
-    <ul
-      className={cn(
-        "flex flex-col absolute top-full right-3 items-center min-w-1/2 md:min-w-auto bg-white px-12 py-6 space-y-6 text-sm md:text-md text-black z-50 drop-shadow-sm",
-        { hidden: !open }
-      )}
-    >
-      <li key="username" className="font-bold">
-        User: {user?.name}
-      </li>
-      <li key="profile">
-        <Link
-          href="/profile-settings"
-          className="font-bold sm:hover:text-brand sm:active:text-brand"
-          onClick={closeMenu}
+function ProfileDropdown({
+  user,
+  open,
+  closeMenu,
+}: {
+  user: User;
+  open: boolean;
+  closeMenu: () => void;
+}) {
+  if (user)
+    return (
+      <ul
+        className={cn(
+          "flex flex-col absolute top-full right-3 items-center min-w-1/2 md:min-w-auto bg-white px-12 py-6 space-y-6 text-sm md:text-md text-black z-50 drop-shadow-sm",
+          { hidden: !open }
+        )}
+      >
+        <li key="username" className="font-bold">
+          User: {user?.name}
+        </li>
+        <li key="profile">
+          <Link
+            href="/profile-settings"
+            className="font-bold sm:hover:text-brand sm:active:text-brand"
+            onClick={closeMenu}
+          >
+            Profile
+          </Link>
+        </li>
+        <li
+          key="logout"
+          onClick={() => {
+            closeMenu();
+            handleLogout(user.id);
+          }}
+          className="font-bold sm:hover:text-brand sm:active:text-brand cursor-pointer"
         >
-          Profile
-        </Link>
-      </li>
-      <li key="logout" onClick={()=>{closeMenu(); handleLogout(user.id)}} className="font-bold sm:hover:text-brand sm:active:text-brand cursor-pointer">
-        Log Out
-      </li>
-    </ul>
-  );
+          Log Out
+        </li>
+      </ul>
+    );
 }
 
 const handleLogout = async (userId: string | null) => {
-  if(userId)
-  try {
-    type RatingsCookie = {
-      ratings: Record<string, number>;
-    };
-    const cookieRaw = Cookies.get("quizRatings");
-    const cookieRatings: RatingsCookie | null = cookieRaw ? JSON.parse(cookieRaw) : null;
-    if(userId && cookieRatings && Object.keys(cookieRatings.ratings).length > 0)
-    await fetch("/api/ratings/toDb", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userId: userId, ratings: cookieRatings.ratings }),
-    });
+  if (userId)
+    try {
+      type RatingsCookie = {
+        ratings: Record<string, number>;
+      };
+      const cookieRaw = Cookies.get("quizRatings");
+      const cookieRatings: RatingsCookie | null = cookieRaw
+        ? JSON.parse(cookieRaw)
+        : null;
+      if (
+        userId &&
+        cookieRatings &&
+        Object.keys(cookieRatings.ratings).length > 0
+      )
+        await fetch("/api/ratings/toDb", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            userId: userId,
+            ratings: cookieRatings.ratings,
+          }),
+        });
 
-    await authClient.signOut();
-  } catch (error) {
-    console.error("Error logging out:", error);
-  }
+      await authClient.signOut();
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
 };
 
-export function Navigation({ user, isPending }: { user: User, isPending: boolean }) {
+export function Navigation({
+  user,
+  isPending,
+  pages,
+}: {
+  user: User;
+  isPending: boolean;
+  pages: NavigationItem[];
+}) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false)
+  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
   const navRef = useRef<HTMLDivElement>(null);
   const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-  const toggleProfileDropdown = () => setIsProfileDropdownOpen(!isProfileDropdownOpen);
+  const toggleProfileDropdown = () =>
+    setIsProfileDropdownOpen(!isProfileDropdownOpen);
   const closeMenu = () => setIsMenuOpen(false);
   const closeProfileDropdown = () => setIsProfileDropdownOpen(false);
 
@@ -310,7 +326,7 @@ export function Navigation({ user, isPending }: { user: User, isPending: boolean
   useClickOutside(navRef, closeProfileDropdown);
 
   const visiblePages = pages.filter((page) => {
-    if (page.path === "/login" && (user || isPending)) return false;
+    if (page.fields.path === "/login" && (user || isPending)) return false;
     return true;
   });
 
@@ -319,31 +335,60 @@ export function Navigation({ user, isPending }: { user: User, isPending: boolean
       className="flex h-14 items-center justify-between px-2 md:px-4 lg:px-8 xl:px-12 relative"
       ref={navRef}
     >
-      <Link href="/" onClick={() => {closeMenu(); closeProfileDropdown();}}>
+      <Link
+        href="/"
+        onClick={() => {
+          closeMenu();
+          closeProfileDropdown();
+        }}
+      >
         <Logo />
       </Link>
 
       {/* Hidden on mobile */}
       <div className="hidden md:flex items-center">
-        <SearchBar closeMenu={() => {closeMenu(); closeProfileDropdown();}} />
+        <SearchBar
+          closeMenu={() => {
+            closeMenu();
+            closeProfileDropdown();
+          }}
+        />
       </div>
       <div className="hidden md:flex items-center space-x-4">
         <ul className="flex justify-center items-center">
-          {visiblePages.map((page, index) => processPage(page, index, closeProfileDropdown))}
+          {visiblePages.map((page, index) =>
+            processPage(page, index, closeProfileDropdown)
+          )}
         </ul>
         {isPending && (
-            <div className="w-8 h-8 border-[3px] border-secondary-text border-t-transparent rounded-full animate-spin" />
-          )
-        }
-        {user && <ProfilePicture user={user} onClick={toggleProfileDropdown}/>}
-        {isProfileDropdownOpen && user && <ProfileDropdown user={user} open={isProfileDropdownOpen} closeMenu={closeProfileDropdown}/>}
+          <div className="w-8 h-8 border-[3px] border-secondary-text border-t-transparent rounded-full animate-spin" />
+        )}
+        {user && <ProfilePicture user={user} onClick={toggleProfileDropdown} />}
+        {isProfileDropdownOpen && user && (
+          <ProfileDropdown
+            user={user}
+            open={isProfileDropdownOpen}
+            closeMenu={closeProfileDropdown}
+          />
+        )}
       </div>
 
       {/* Visible on mobile */}
       <div className="flex md:hidden space-x-2 items-center">
         <div className="flex space-x-2">
-          <SearchBar closeMenu={() => {closeMenu(); closeProfileDropdown();}} />
-          <Hamburger isOpen={isMenuOpen} toggleMenu={() => {closeProfileDropdown(); toggleMenu()}} />
+          <SearchBar
+            closeMenu={() => {
+              closeMenu();
+              closeProfileDropdown();
+            }}
+          />
+          <Hamburger
+            isOpen={isMenuOpen}
+            toggleMenu={() => {
+              closeProfileDropdown();
+              toggleMenu();
+            }}
+          />
         </div>
 
         <ul
@@ -352,15 +397,30 @@ export function Navigation({ user, isPending }: { user: User, isPending: boolean
             { hidden: !isMenuOpen }
           )}
         >
-          {visiblePages.map((page, index) => processPage(page, index, closeMenu))}
+          {visiblePages.map((page, index) =>
+            processPage(page, index, closeMenu)
+          )}
         </ul>
 
         {isPending && (
-            <div className="w-8 h-8 border-[3px] border-secondary-text border-t-transparent rounded-full animate-spin" />
-          )
-        }
-        {user && <ProfilePicture user={user} onClick={() => {closeMenu(); toggleProfileDropdown()}}/>}
-        {isProfileDropdownOpen && user && <ProfileDropdown user={user} open={isProfileDropdownOpen} closeMenu={closeProfileDropdown}/>}
+          <div className="w-8 h-8 border-[3px] border-secondary-text border-t-transparent rounded-full animate-spin" />
+        )}
+        {user && (
+          <ProfilePicture
+            user={user}
+            onClick={() => {
+              closeMenu();
+              toggleProfileDropdown();
+            }}
+          />
+        )}
+        {isProfileDropdownOpen && user && (
+          <ProfileDropdown
+            user={user}
+            open={isProfileDropdownOpen}
+            closeMenu={closeProfileDropdown}
+          />
+        )}
       </div>
     </nav>
   );
